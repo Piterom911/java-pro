022e46bceea1ab1cf1fd09e19b5a7d28dce6764e
Roman Dogoda <piterom911@gmail.com>
2024-12-04 13:14:12 +0100

    recursion fibonacci

---
+++ src/main/java/com/telran/algorithm/algorithm04/Fibonacci.java
@@ -0,0 +1,25 @@
+package com.telran.algorithm.algorithm04;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int num = 8;
+
+        System.out.println(findFibonacci(num));
+
+    }
+
+    private static int findFibonacci(int index) {
+        int result = 0;
+        int first = 0;
+        int second = 1;
+
+        for (int i = 0; i < index ; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+
+        return result;
+    }
+}
