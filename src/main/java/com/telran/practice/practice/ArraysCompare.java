1d4a9c930c2d22ef65595ec88553ff939c0b3e58
Roman Dogoda <piterom911@gmail.com>
2024-12-18 09:35:56 +0100

    lesson 14 practice

---
+++ src/main/java/com/telran/practice/practice/ArraysCompare.java
@@ -0,0 +1,13 @@
+package com.telran.practice.practice;
+
+// 2. Проверьте, равны ли два массива или нет (массивы считаются равными, если содержат одинаковые элементы, порядок не важен)
+public class ArraysCompare {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6,};
+        int[] arr2 = {1, 2, 3, 4, 5, 6,};
+
+
+    }
+
+}
---
+++ src/main/java/com/telran/practice/practice/FirstElement.java
@@ -0,0 +1,15 @@
+package com.telran.practice.practice;
+
+public class FirstElement {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 6, 4, 7, 3, 1, 7, 6, 9, 0};
+        int k = 4;
+
+        int result = findFirstDoubled(arr, k);
+    }
+
+    private static int findFirstDoubled(int[] arr, int k) {
+        return 0;
+    }
+}
