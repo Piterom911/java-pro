6b4a753c7a8ba84851abd8d997c2141e40366a22
Roman Dogoda <piterom911@gmail.com>
2024-12-19 09:47:55 +0100

    algorithms QuickSort and ReverseNumber without String

---
+++ src/main/java/com/telran/algorithm/algorithm06/ReverseNumber.java
@@ -0,0 +1,28 @@
+package com.telran.algorithm.algorithm06;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        int num = -120;
+        System.out.println(reverseNum(num));
+    }
+
+    public static int reverseNum(int num) {
+        int result = 0;
+//        boolean isNegative = num < 0;
+        int temp = 1;
+
+        if (num < 0) {
+            num *= -1;
+            temp = -1;
+        }
+
+        while (num > 0) {
+            int firstNum = num % 10;
+            result = result * 10 + firstNum ;
+            num = num / 10;
+        }
+
+        return result * temp;
+    }
+}
