e6d1621a107e75406c578806474dc3003feaf568
Roman Dogoda <piterom911@gmail.com>
2025-01-13 10:45:15 +0100

    Test git functionality

---
+++ src/main/java/com/telran/practice/practice/Test.java
@@ -0,0 +1,14 @@
+package com.telran.practice.practice;
+
+// This one is just a test to check git functionality
+
+public class Test {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello my friend!!!");
+    }
+}
